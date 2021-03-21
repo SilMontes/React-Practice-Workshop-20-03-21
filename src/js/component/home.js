41111211275051b73adfa6efd84352a376045182
SilMontes <Silmontes2001@gmail.com>
2021-03-21 05:24:28 +0000

    adding example props

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,15 +1,38 @@
 import React from "react";
-
+import PropTypes from "prop-types";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-export function Home() {
+const Container = () => {
+	return (
+		<React.Fragment>
+			<Home title="Hello" image={rigoImage} />
+			<Home
+				title="Second Hello"
+				image={
+					"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQq5HKvZ8puTuFmDRyALrSmjlIhonjV0z261A&usqp=CAU"
+				}
+			/>
+			<Home
+				title="Third Hello"
+				image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKlwJRgFjsLre-AP66xe1vr4HnnYBIgCyONg&usqp=CAU"
+			/>
+		</React.Fragment>
+	);
+};
+function Home(props) {
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
+			<h1>{props.title}</h1>
 			<p>
-				<img src={rigoImage} />
+				<img
+					src={
+						props.image === ""
+							? "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpQsLPvR8rSuqUhZlzexUmpk1qzfaGcdwGAQ&usqp=CAU"
+							: props.image
+					}
+				/>
 			</p>
 			<a href="#" className="btn btn-success">
 				If you see this green button... bootstrap is working
@@ -22,3 +45,9 @@ export function Home() {
 		</div>
 	);
 }
+Home.propTypes = {
+	title: PropTypes.string,
+	image: PropTypes.image
+};
+
+export default Container;
